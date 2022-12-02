6cfa0446bfc2fb289ed49b9537d5392c4262cb0b
Jonathan Rangel <jhonjonathan81@gmail.com>
2022-12-02 14:29:05 -0400

    cambios 2.0

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from usuarios import usuario
 
-def createVen():
+def principal():
     
     root = Tk()
     root.title('Programa de Entrenamiento')
@@ -40,17 +40,64 @@ def createVen():
     
     root.mainloop()
 
+def registro():     
+    root = Tk()
+    root.title('Resitro de datos')
+
+    mainFrame = Frame(root)
+    mainFrame.pack()
+    mainFrame.config(width = 400 , height= 400)
+
+    titulo = Label(mainFrame, text = " Registro ", font = ("Times New Roman", 12))
+    titulo.grid(column = 0,  row = 0, padx= 5, pady = 5, columnspan = 2)
+
+    #Usuario y contraseña
+    userLabel = Label(mainFrame, text = "Nombre: ")
+    userLabel.grid(column= 0 , row = 1)
+    
+    passLabel = Label(mainFrame, text = "Contraseña: ")
+    passLabel.grid(column= 0 , row = 2)
+    
+    userLabel = Label(mainFrame, text = "Peso ")
+    userLabel.grid(column= 0 , row = 3)
+    
+    passLabel = Label(mainFrame, text = "Altura: ")
+    passLabel.grid(column= 0 , row = 4)
+
+    #Entrada de datos
+    nombreUser = StringVar()
+    nombreUser.set('')
+    nombreEntry = Entry(mainFrame, textvariable = nombreUser)
+    nombreEntry.grid(column = 1, row = 1)
+
+    passUser = StringVar()
+    passUser.set('')
+    passEntry = Entry(mainFrame, textvariable = passUser, show = '*')
+    passEntry.grid(column = 1, row = 2)
+    
+    pesoUser = StringVar()
+    pesoUser.set('')
+    pesoEntry = Entry(mainFrame, textvariable = pesoUser)
+    pesoEntry.grid(column = 1, row = 3)
+
+    alturaUser = StringVar()
+    alturaUser.set('')
+    alturaEntry = Entry(mainFrame, textvariable = alturaUser)
+    alturaEntry.grid(column = 1, row = 4)
+
+    #Boton
+    resgisButton = Button(mainFrame, text = 'Registro', command = validarRegistro)
+    resgisButton.grid(column = 1, row = 5, ipadx = 2, padx = 5, pady = 8)
+    
+    root.mainloop()
+
+
 def iniciar():
-    user1.conectar
-
-def registro():
-    pass
-
-if __name__ == "__main__":
+    print("Conectar")
     
-    # user1 = usuario(input('Ingrese su nombre: '), input('Ingrese contraseña: '))
-    user1 = usuario("Jhonny","123")
+def validarRegistro():
+    print("Validar")
     
-    createVen()
- 
+    
 
+principal()
